46288e0036f7d14b5b289898d099a52c782df682
Alireza Nasri <gg.nasri@gmail.com>
2021-12-22 16:30:01 +0330

    set load_config_file= false

--- deployment/terraform/helm.tf
+++ deployment/terraform/helm.tf
@@ -4,6 +4,7 @@ provider "helm" {
     host                   = data.aws_eks_cluster.eks.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
     token                  = data.aws_eks_cluster_auth.eks.token
+    load_config_file       = false
   }
 }
 resource "helm_release" "nginx_ingress" {
